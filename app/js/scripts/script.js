6b87921fcf8f68ae3d86796d581ff99b2cc4bf8b
Ann Semenova <semenova.7oa@yandex.ru>
2018-08-03 14:19:42 +0300

    header

--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -1,4 +1,23 @@
 // js jquery
 $(document).ready(function() {
+    //клонируем из мобильной для адаптива
+    $('.application-btn').clone().appendTo('.header__application');
 
+    //кнопка бургер
+    $('.js-menu-btn').click(function(){
+        $(this).addClass('open');
+        $('.js-menu').slideDown();
+    });
+
+    //закрыть моб. меню
+    $('.js-menu-close').click(function(){
+        $('.js-menu').slideUp();
+        $('.js-menu-btn').removeClass('open');
+    });
+
+    //выпадающее меню
+    $('.js-menu-link').click(function () {
+        $(this).next().slideToggle();
+        return false;
+    });
 });
