789ab239c65be962fea864487bf76c2b834c97b9
Ann Semenova <semenova.7oa@yandex.ru>
2018-08-08 13:12:33 +0300

    new

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,6 +1,8 @@
 // Plugins
 //= plugins/_jquery-3.2.0.min.js
 //= plugins/_slick.min.js
+//= plugins/_sly-plugins.js
+//= plugins/_sly.min.js
 
 //Scripts
 //= scripts/script.js
--- app/js/scripts/script.js
+++ app/js/scripts/script.js
@@ -4,7 +4,7 @@ $(window).on('load resize', function(){
     isModile = !window.matchMedia('(min-width: 680px)').matches;
 });
 $(document).ready(function() {
-    //клонируем из мобильной для адаптива
+    //клонируем для адаптива
     $('.application-btn').clone().appendTo('.header__application');
     $('.ss-list').clone().appendTo('.footer__desktop-ss');
     $('.footer__application .application-btn-footer').clone().appendTo('.footer__tablet-application');
@@ -35,7 +35,7 @@ $(document).ready(function() {
     });
 
     //слайдер объектов
-    $('.obj-slider').slick({
+    /*$('.obj-slider').slick({
         dots: true,
         infinite: false,
         slidesToShow: 2,
@@ -46,6 +46,27 @@ $(document).ready(function() {
                 settings: "unslick"
             }
         ]
+    });*/
+    $('.obj-slider').each(function(){
+        var frame  = $(this);
+        var wrap   = frame.parent();
+
+        frame.sly({
+            horizontal: 1,
+            itemNav: 'basic',
+            smart: 1,
+            scrollBar: wrap.find('.sly-scrollbar'),
+            scrollBy: 0,
+            pagesBar: wrap.find('.sly-pages'),
+            activatePageOn: 'click',
+            speed: 300,
+            dragHandle: 1,
+            clickBar: 1,
+
+            prevPage: wrap.find('.prevPage'),
+            nextPage: wrap.find('.nextPage')
+        });
     });
+
 
 });
